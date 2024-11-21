63c1c9820dca5c99c0fd3ca20e1cd8252638a81d
unknown <a.cina@procreatech.com>
2024-11-21 13:58:02 +0100

    7. Consuming Observable-based services using the Angular async Pipe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,9 +23,13 @@ import { CoursesService } from "../services/courses.service";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  beginnerCourses: Course[];
+  // in un componente Observable-based le proprietà sono degli Observables
+  // derivati dall'observable courses$ e verranno sottoscritti ed utilizzati direttamente nella view tranmite pipe async
+  // beginnerCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
+  // advancedCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
   // l'HttpClient non serve più qui, serve iniettare il CoursesService
   // constructor(private http: HttpClient, private dialog: MatDialog) {}
@@ -49,6 +53,31 @@ export class HomeComponent implements OnInit {
     //     (course) => course.category == "ADVANCED"
     //   );
     // });
+
+    // chiamiamo il metodo loadAllCourses e riceviamo un observable
+    // fino a che non facciamo il subscribe ad un observable non abbiamo il dato, l'observable emette il dato solo quando viene sottoscritto
+    // per coinvenzione si utilizza il dollaro alla fine di un nome di variabile per capire subito che si tratta di un Observable
+    // il sort dei Course lo facciamo una volta sola qui, dopo saranno già ordinati
+    const courses$: Observable<Course[]> = this.coursesService
+      .loadAllCourses()
+      .pipe(map((courses) => courses.sort(sortCoursesBySeqNo)));
+
+    courses$.subscribe((val) => console.log(val));
+
+    // tramite map assegnamo a beginnersCourses$ un observable con un array di Course di sola categoria BEGINNER ordinati in modo ascendente secondo seqNo
+    this.beginnerCourses$ = courses$.pipe(
+      map(
+        (courses) => courses.filter((course) => course.category === "BEGINNER")
+        // .sort(sortCoursesBySeqNo)
+      )
+    );
+
+    // stessa cosa per i corsi di categoria ADVANCED
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === "ADVANCED")
+      )
+    );
   }
 
   editCourse(course: Course) {
