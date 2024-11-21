4ad9a051ed4a1d3201ae3a15d63c15a796491f57
unknown <a.cina@procreatech.com>
2024-11-21 12:57:21 +0100

    6. Design Pattern - Stateless Observable-based Services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,46 +15,40 @@ import {
 import { HttpClient } from "@angular/common/http";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
+import { CoursesService } from "../services/courses.service";
 
 @Component({
   selector: "home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
-
-// in un componente scritto così c'è troppa logica
-// si inietta l'HttpService e si fa la chiamata qui, si gestiscono i dati ricevuti qui
-// il componente non dovrebbe sapere da dove provengono i dati, dovrebbe solo utilizzarli
-// la logica descritta qui non è riutilizzabile da nessuna altra parte
-// se cambiassimo i dati in beginner ed advanced questi cambiamenti sarebbero disponibili solo all'interno di questo componente
-// bisognba fare in modo che gli stessi dati, ed i loro cambiamenti, siano disponibili in tutta l'app
 export class HomeComponent implements OnInit {
   beginnerCourses: Course[];
 
   advancedCourses: Course[];
 
-  constructor(private http: HttpClient, private dialog: MatDialog) {}
+  // l'HttpClient non serve più qui, serve iniettare il CoursesService
+  // constructor(private http: HttpClient, private dialog: MatDialog) {}
+  constructor(
+    private coursesService: CoursesService,
+    private dialog: MatDialog
+  ) {}
 
+  // spostiamo la logica che ora è nel componente in un service:
+  // ng g s services/courses --project reactive-angular-course --skip-tests
   ngOnInit() {
-    // tutte le chiamate dovrebbero iniziare con http://localhost:9000, ma le possiamo fare così perchè il comando strat è runnato per utilizzare un proxy configurato nel file proxy.json
-    // nel proxy diciamo che tutte le rotte che iniziano con /api le riferiamo a http://localhost:9000, quindi il proxy completa l'url per noi
-    this.http.get("/api/courses").subscribe((res) => {
-      // riceviamo l'array di corsi, estraiamo la property payload che è un altro array di oggetti
-      // la proprietà payload c'è perchè la mette lui quando risponde (vedere get-courses.route.ts)
-      //  res.status(200).json({payload:Object.values(COURSES)});
-      // la property payload ha come value un array, questo viene ordinato secondo un ordine
-      const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
-
-      // tramite metodo filetr() ottengo 2 array secondo categoria
-      // il codice dovrebbe essere scritto in maniera tale da evitare callback innestate, in modo da evitare il cosiddetto callback hell
-      this.beginnerCourses = courses.filter(
-        (course) => course.category == "BEGINNER"
-      );
-
-      this.advancedCourses = courses.filter(
-        (course) => course.category == "ADVANCED"
-      );
-    });
+    // chiamiamo il metodo per fetchare i Course dal BE implementato nel CoursesService
+    // il nostro componente non sa da dove provengono i dati, li riceve e basta
+    //questa non serve più, non chiamiamo direttamente da qui il BE
+    // this.http.get("/api/courses").subscribe((res) => {
+    //   const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
+    //   this.beginnerCourses = courses.filter(
+    //     (course) => course.category == "BEGINNER"
+    //   );
+    //   this.advancedCourses = courses.filter(
+    //     (course) => course.category == "ADVANCED"
+    //   );
+    // });
   }
 
   editCourse(course: Course) {
