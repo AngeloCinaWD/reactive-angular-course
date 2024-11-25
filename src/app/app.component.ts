9270e99f49b3539969a5b0732cb37e7f11d9a4e1
unknown <a.cina@procreatech.com>
2024-11-25 21:01:34 +0100

    28. Adapting the UI according to the user Authentication status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { LoadingService } from "./services/loading.service";
 import { MessagesService } from "./services/messages.service";
+import { AuthStore } from "./services/auth.store";
 
 @Component({
   selector: "app-root",
@@ -8,9 +9,13 @@ import { MessagesService } from "./services/messages.service";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  // iniettiamo l'AuthStore
+  constructor(public authStore: AuthStore) {}
 
   ngOnInit() {}
 
-  logout() {}
+  logout() {
+    // per il logout basta chiamare questo metodo
+    this.authStore.logout();
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { Router } from "@angular/router";
+import { AuthStore } from "../services/auth.store";
 
 // creiamo un nuovo file store per gestire l'autenticazione di un utente, quindi le operazioni di login e logout
 // creiamo il file services/auth.store.ts
@@ -13,9 +14,14 @@ import { Router } from "@angular/router";
 export class LoginComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  // iniettiamo l'AuthStore per poterne utilizzare le API per gestire l'autenticazione
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authStore: AuthStore
+  ) {
     this.form = fb.group({
-      email: ["test@angular-university.io", [Validators.required]],
+      email: ["test@angular-university.i", [Validators.required]],
       password: ["test", [Validators.required]],
     });
   }
@@ -23,6 +29,19 @@ export class LoginComponent implements OnInit {
   ngOnInit() {}
 
   login() {
+    // questa const contiene email e password
     const val = this.form.value;
+
+    // chiamiamo il metodo login() dell'AuthStore e gli passiamo email e password
+    // dobbiamo fare il subscribe perchè è un Observable
+    this.authStore.login(val.email, val.password).subscribe(
+      // se il login ha successo navighiamo verso la pagina con i Course
+      () => this.router.navigateByUrl("/courses"),
+      // se fallisce avrò un error ed avviso con un alert()
+      (err) => {
+        console.log(err);
+        alert("Login failed");
+      }
+    );
   }
 }
